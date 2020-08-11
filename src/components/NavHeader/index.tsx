09c693f9fefc5152170cb8309b71c1fc705f52e8
Luis Sousa <lv-ss@hotmail.com>
2020-08-10 22:48:40 -0300

    finalizando landing page v2.0

---
+++ src/components/NavHeader/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css'
+
+const NavHeader: React.FC = (props) => {
+  return (
+    <nav className='nav-header'>
+      <div className='nav-header-content'>
+        {props.children}
+      </div>
+    </nav>
+  );
+}
+
+export default NavHeader;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { FiPower } from 'react-icons/fi'
 
 import './styles.css'
 import logoImg from '../../assets/images/logo.svg'
@@ -8,6 +9,7 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import api from '../../services/api'
+import NavHeader from '../../components/NavHeader'
 
 function Landing(){
   const [totalConnections, setTotalConnections] = useState(0)
@@ -22,6 +24,13 @@ function Landing(){
   return (
     <div id="page-landing">
       <div className="container" id="page-landing-container">
+      <NavHeader>
+        <div className="user-info">
+          <img src='https://avatars2.githubusercontent.com/u/48053804?s=460&u=41a09241f1f585f000c86e67bf3b6147aed66ed6&v=4' alt=''/>
+          <p>Luis sousa</p>
+        </div>
+        <button className='login-button'><FiPower size={20} className='icon'/></button>
+      </NavHeader>
         <div className="logo-container">
           <img src={logoImg} alt="Proffy"/>
           <h2>Sua plataforma de estudos online</h2>
@@ -32,6 +41,13 @@ function Landing(){
           alt="Plataforma de Estudos" 
           className="hero-image"
         />
+
+        <div className='welcome-text'>
+          <p>Seja bem-vindo, <br/>
+            <strong>O que deseja Fazer?</strong>
+          </p>
+        </div>
+
         <div className="buttons-container">
           <Link to="/study" className="study">
             <img src={studyIcon} alt="estudar"/>
